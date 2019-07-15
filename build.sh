4f2975564738ecbb2f8ea27a89e982190fded051
Kenneth Durbrow <durbrow@users.noreply.github.com>
2019-07-15 10:53:45 -0400

    cleanup

--- build.sh
+++ build.sh
@@ -1,42 +1,38 @@
 #!/bin/bash
-
+CONFIGURE="./configure --without-debug"
 rm -rf ngs ncbi-vdb sra-tools ncbi-outdir
 
 # get fresh repo's
-git clone --branch engineering https://github.com/ncbi/ngs.git    
-git clone https://github.com/ncbi/ncbi-vdb.git
-git clone https://github.com/ncbi/sra-tools.git
+git clone --branch engineering --depth 1 https://github.com/ncbi/ngs.git
+git clone --branch engineering --depth 1 https://github.com/ncbi/ncbi-vdb.git
+git clone --branch engineering --depth 1 https://github.com/ncbi/sra-tools.git
 
 cd ncbi-vdb
-git checkout engineering
 
 echo "building engineering branch ..."
-./configure >/dev/null 2>&1 || { echo "configure script failed"; exit 1; }
+${CONFIGURE} >/dev/null 2>&1 || { echo "configure script failed"; exit 1; }
 make -sj1 >/dev/null 2>&1 || { echo "make failed"; exit 1; }
 
-echo "creating integration branch ..."
-git checkout -b integration
-
-echo "built integration branch in ncbi-vdb"
+echo "built engineering branch in ncbi-vdb"
 cd ..
 
 
 echo "building ngs-sdk library ..."
 cd ngs
-./configure >/dev/null 2>&1 || { echo "configure script failed"; exit 1; }
+${CONFIGURE} >/dev/null 2>&1 || { echo "configure script failed"; exit 1; }
 make -sj1 -C ngs-sdk >/dev/null 2>&1 || { echo "make failed"; exit 1; }
 cd ..
 
 
 cd sra-tools
-git checkout engineering
 
 echo "building engineering branch ..."
-./configure >/dev/null 2>&1 || { echo "configure script failed"; exit 1; }
+${CONFIGURE} >/dev/null 2>&1 || { echo "configure script failed"; exit 1; }
 make -sj1 >/dev/null 2>&1 || { echo "make failed"; exit 1; }
 
-echo "creating integration branch ..."
-git checkout -b integration
-
-echo "built integration branch in sra-tools"
+echo "built engineering branch in sra-tools"
+echo "installing..."
+sudo rm -rf /etc/ncbi /etc/profile.d/sra-tools.* /usr/local/ncbi
+sudo make install
 cd ..
+rm -rf ngs ncbi-vdb sra-tools ncbi-outdir .ncbi SRR* ERR* DRR*
